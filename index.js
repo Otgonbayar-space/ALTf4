ab0050086b31946c9b25066f1b02c97a7c477c8e
BACK-14 <back-14@Erxess-Mac-mini.local>
2025-10-23 20:51:10 +0800

    login

---
+++ index.js
@@ -0,0 +1,19 @@
+const auth = () => {
+    username= 'admin@erxes.io',
+    password= '1234'
+}
+const inpt = () => {
+const usernameInput = document.getElementById('username')
+const passwordInput = document.getElementById('password')
+}
+const LoginButton = document.getElementById("Login");
+
+
+const Login = () => {
+    If ( auth === inpt)
+    console.log('succesful');
+} 
+console.log('unsuccesful')
+
+ 
+LoginButton.addEventListener("click", Login);
